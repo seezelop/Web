2f618ac8bcb33bb44c5680e36fe3093931a6916b
Sebastian <sebastianlopez1984@gmail.com>
2022-09-22 19:25:26 -0300

    comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- ! emmet genero codigo html de forma automatica e instale plugin de Bootstrap 4--> 
+<?php include_once("template/cabecera.php"); ?>
+            
+            <div class="jumbotron">
+                <h1 class="display-3">Inicio</h1>
+                <p class="lead">Inicio del sitio web</p>
+                <hr class="my-2">
+                <p>Bienvenidos a nuestro sitio web.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+            
+<?php include_once("template/pie.php"); ?>      
+       
+
